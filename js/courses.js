fb6b498cb466d09854f3d2cbc41f73ee2f6c0f88
PedroFFigueiredo <pedrohenrifagundesfigueiredo@gmail.com>
2022-11-06 13:26:01 -0300

    Criação da seção nossos cursos

--- js/courses.js
+++ js/courses.js
@@ -1,7 +1,80 @@
-function seeCourses() {
-    const botaoVerCursos = document.querySelector(".school-front-end");
-    botaoVerCursos.classList.add('active');
+function seeCoursesFront() {
+    const verColunaUmFront = document.querySelector(".colun-one-front");
+    verColunaUmFront.classList.add('active');
 
-    // const botaoFecharCursos = document.querySelector(".school-front-end");
-    // botaoFecharCursos.classList.remove('active');
+    const verColunaDoisFront = document.querySelector(".colun-two-front");
+    verColunaDoisFront.classList.add('active');
+
+    const fecharColunaUmBack = document.querySelector(".colun-one-back");
+    fecharColunaUmBack.classList.remove('active');
+
+    const fecharColunaDoisBack = document.querySelector(".colun-two-back");
+    fecharColunaDoisBack.classList.remove('active');
+
+    const fecharColunaUmData = document.querySelector(".colun-one-data");
+    fecharColunaUmData.classList.remove('active');
+
+    const FecharColunaUmMobile = document.querySelector(".colun-one-mobile");
+    FecharColunaUmMobile.classList.remove('active');
+}
+
+function seeCoursesBack() {
+    const verColunaUmBack = document.querySelector(".colun-one-back");
+    verColunaUmBack.classList.add('active');
+
+    const verColunaDoisBack = document.querySelector(".colun-two-back");
+    verColunaDoisBack.classList.add('active');
+
+
+    const fecharColunaUmFront = document.querySelector(".colun-one-front");
+    fecharColunaUmFront.classList.remove('active');
+
+    const fecharColunaDoisFront = document.querySelector(".colun-two-front");
+    fecharColunaDoisFront.classList.remove('active');
+
+    const fecharColunaUmData = document.querySelector(".colun-one-data");
+    fecharColunaUmData.classList.remove('active');
+
+    const FecharColunaUmMobile = document.querySelector(".colun-one-mobile");
+    FecharColunaUmMobile.classList.remove('active');
+}
+
+function seeCoursesData() {
+    const verColunaUmData = document.querySelector(".colun-one-data");
+    verColunaUmData.classList.add('active');
+
+    const fecharColunaUmFront = document.querySelector(".colun-one-front");
+    fecharColunaUmFront.classList.remove('active');
+
+    const fecharColunaDoisFront = document.querySelector(".colun-two-front");
+    fecharColunaDoisFront.classList.remove('active');
+
+    const fecharColunaUmBack = document.querySelector(".colun-one-back");
+    fecharColunaUmBack.classList.remove('active');
+
+    const fecharColunaDoisBack = document.querySelector(".colun-two-back");
+    fecharColunaDoisBack.classList.remove('active');
+
+    const FecharColunaUmMobile = document.querySelector(".colun-one-mobile");
+    FecharColunaUmMobile.classList.remove('active');
+}
+
+function seeCoursesMobile() {
+    const verColunaUmMobile = document.querySelector(".colun-one-mobile");
+    verColunaUmMobile.classList.add('active');
+
+    const fecharColunaUmFront = document.querySelector(".colun-one-front");
+    fecharColunaUmFront.classList.remove('active');
+
+    const fecharColunaDoisFront = document.querySelector(".colun-two-front");
+    fecharColunaDoisFront.classList.remove('active');
+
+    const fecharColunaUmBack = document.querySelector(".colun-one-back");
+    fecharColunaUmBack.classList.remove('active');
+
+    const fecharColunaDoisBack = document.querySelector(".colun-two-back");
+    fecharColunaDoisBack.classList.remove('active');
+
+    const fecharColunaUmData = document.querySelector(".colun-one-data");
+    fecharColunaUmData.classList.remove('active');
 }
