331d0563188c68ed3880508a8c22b270eca5d2a7
PedroFFigueiredo <pedrohenrifagundesfigueiredo@gmail.com>
2022-11-05 21:56:29 -0300

    Alterações

---
+++ js/courses.js
@@ -0,0 +1,7 @@
+function seeCourses() {
+    const botaoVerCursos = document.querySelector(".school-front-end");
+    botaoVerCursos.classList.add('active');
+
+    // const botaoFecharCursos = document.querySelector(".school-front-end");
+    // botaoFecharCursos.classList.remove('active');
+}
